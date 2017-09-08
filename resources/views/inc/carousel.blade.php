41de36250caa5c83613dcc772a239c67bdd1f39f
LAPTOP-JAL999HV\Michael Schell <michael-schell@hotmail.com>
2017-09-08 10:26:05 +0200

    okay!

--- resources/views/inc/carousel.blade.php
+++ resources/views/inc/carousel.blade.php
@@ -1,65 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-      <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
-      <link rel="stylesheet" href="{{ asset('bootstrap/js/bootstrap.js') }}">
-      <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-</head>
-<body>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-
-      <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-
-      <div class="carousel-inner" role="listbox">
-
-          <div class="item active">
-              <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First Slide">
-              <div class="container">
-                  <div class="carousel-caption">
-                      <h1>Test Header</h1>
-                      <p>Input Text Here</p>
-                  </div>
-              </div>
-          </div>
-
-          <div class="item">
-              <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second Slide">
-              <div class="container">
-                  <div class="carousel-caption">
-                      <h1>Test Header 2</h1>
-                      <p>Input Text Here</p>
-                  </div>
-              </div>
-          </div>
-
-          <div class="item">
-              <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third Slide">
-              <div class="container">
-                  <div class="carousel-caption">
-                      <h1>Test Header 3</h1>
-                      <p>Input Text Here</p>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-      </a>
-
+<div class="container">
+  <div class="carousel">
+    <input type="radio" id="carousel-1" name="carousel[]" checked>
+    <input type="radio" id="carousel-2" name="carousel[]">
+    <input type="radio" id="carousel-3" name="carousel[]">
+    <input type="radio" id="carousel-4" name="carousel[]">
+    <input type="radio" id="carousel-5" name="carousel[]">
+    <ul class="carousel__items">
+      <li class="carousel__item"><img src="//lh5.googleusercontent.com/-cTEgPOnd3l8/U8-EmaZ4KNI/AAAAAAAABc8/6eacbALkQ6A/w1358-h905-no/carousel-1.JPG" alt=""></li>
+      <li class="carousel__item"><img src="//lh4.googleusercontent.com/-ntVHbbWX5eo/U8-EmV8P4cI/AAAAAAAABc4/ICYBGkcztTc/w1358-h905-no/carousel-2.jpg" alt=""></li>
+      <li class="carousel__item"><img src="//lh5.googleusercontent.com/-batEXUZE_e4/U8-EmLF9-hI/AAAAAAAABc0/J3tJVUa6Buk/w1358-h905-no/carousel-3.jpg" alt=""></li>
+      <li class="carousel__item"><img src="//lh5.googleusercontent.com/-gywqIeMvel0/U8-EolKdtkI/AAAAAAAABdM/G0-NHuvvJUU/w1358-h905-no/carousel-4.jpg" alt=""></li>
+      <li class="carousel__item"><img src="//lh5.googleusercontent.com/--2iANjL3ikc/U8-EoGJ18mI/AAAAAAAABdI/fBe-q3Gos6Y/w1358-h905-no/carousel-5.jpg" alt=""></li>
+    </ul>
+    <div class="carousel__prev">
+      <label for="carousel-1"></label>
+      <label for="carousel-2"></label>
+      <label for="carousel-3"></label>
+      <label for="carousel-4"></label>
+      <label for="carousel-5"></label>
+    </div>
+    <div class="carousel__next">
+      <label for="carousel-1"></label>
+      <label for="carousel-2"></label>
+      <label for="carousel-3"></label>
+      <label for="carousel-4"></label>
+      <label for="carousel-5"></label>
+    </div>
+    <div class="carousel__nav">
+      <label for="carousel-1"></label>
+      <label for="carousel-2"></label>
+      <label for="carousel-3"></label>
+      <label for="carousel-4"></label>
+      <label for="carousel-5"></label>
+    </div>
   </div>
-
-<link rel="script" href="{{ asset('jquery.js') }}">
-<link rel="script" href="{{ asset('bootstrap/js/bootstrap.js') }}">
-
-</body>
-</html>
+</div>
